60fad01b7c9f996e6707579a3430a719e3292d0f
Camillashodi <camillashodi@gmail.com>
2022-04-28 16:23:34 +0300

    js and pp

--- source/js/main.js
+++ source/js/main.js
@@ -8,6 +8,139 @@ window.addEventListener('DOMContentLoaded', () => {
   // Utils
   // ---------------------------------
 
+  const textButton = document.querySelector('[data-text-btn]');
+  const deskText = document.querySelector('[data-desk-text]');
+
+  if (textButton && deskText) {
+    textButton.classList.remove('is-open');
+    deskText.classList.remove('is-open');
+
+    textButton.addEventListener('click', function () {
+      if (!deskText.classList.contains('is-open')) {
+        deskText.classList.add('is-open');
+        if (!textButton.classList.contains('is-open')) {
+          textButton.classList.add('is-open');
+          textButton.innerHTML = 'Свернуть';
+        }
+      } else {
+        if (deskText.classList.contains('is-open')) {
+          deskText.classList.remove('is-open');
+          if (textButton.classList.contains('is-open')) {
+            textButton.classList.remove('is-open');
+            textButton.innerHTML = 'Подробнее';
+          }
+        }
+      }
+    });
+  }
+
+  const toggles = document.querySelectorAll('[data-toggle]');
+  const footerContents = document.querySelectorAll('[data-footer-content]');
+
+  if (toggles.length !== 0) {
+    if (footerContents.length !== 0) {
+      footerContents.forEach((content) => {
+        content.classList.remove('no-js');
+      });
+
+      toggles.forEach((toggle) => {
+        toggle.classList.remove('no-js');
+      });
+    }
+
+    const removeClassIsOpen = (elements) => {
+      elements.forEach((element) => {
+        element.classList.remove('is-open');
+      });
+    };
+
+    toggles.forEach((toggle) => {
+      toggle.addEventListener('click', function () {
+        if (!toggle.classList.contains('is-open')) {
+          removeClassIsOpen(toggles);
+          if (footerContents.length !== 0) {
+            removeClassIsOpen(footerContents);
+          }
+          toggle.classList.add('is-open');
+
+          footerContents.forEach((content) => {
+            if (content.dataset.footerContent === toggle.dataset.toggle) {
+              content.classList.add('is-open');
+            }
+          });
+        } else {
+          toggle.classList.remove('is-open');
+
+          if (footerContents.length !== 0) {
+            footerContents.forEach((content) => {
+              if (content.dataset.footerContent === toggle.dataset.toggle) {
+                content.classList.remove('is-open');
+              }
+            });
+          }
+        }
+      });
+    });
+  }
+
+  const inputs = document.querySelectorAll('[data-input-phone]');
+
+  const prefixNumber = (str) => {
+    if (str === '7') {
+      return '7 (';
+    }
+    /*
+    if (str === '8') {
+      return '8 (';
+    }*/
+    if (str === '9') {
+      return '7 (9';
+    }
+    return '7 (';
+  };
+
+  // ======================================
+  inputs.forEach((input) => {
+    input.addEventListener('input', () => {
+      const value = input.value.replace(/\D+/g, '');
+      const numberLength = 11;
+
+      let result;
+      if (input.value.includes('+8') || input.value[0] === '8') {
+        result = '';
+      } else {
+        result = '+';
+      }
+
+      //
+      for (let i = 0; i < value.length && i < numberLength; i++) {
+        switch (i) {
+          case 0:
+            result += prefixNumber(value[i]);
+            continue;
+          case 4:
+            result += ') ';
+            break;
+          case 7:
+            result += '-';
+            break;
+          case 9:
+            result += '-';
+            break;
+          default:
+            break;
+        }
+        result += value[i];
+      }
+      //
+      input.value = result;
+    });
+  });
+
+
+  // ======================================
+
+
   iosVhFix();
 
   // Modules
